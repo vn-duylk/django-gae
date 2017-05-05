e256061564fe4078ffabe143a52568ae7597c424
duylk <duylk@aoi-sys.vn>
2017-05-05 09:50:34 +0700

    commit .py and .html files

--- guestbook/forms.py
+++ guestbook/forms.py
@@ -3,7 +3,3 @@ from django import forms
 class SignForm(forms.Form):
 	name = forms.CharField(label='Name', max_length=100)
 	message = forms.CharField(widget=forms.Textarea, max_length=1000)
-
-
-
-
