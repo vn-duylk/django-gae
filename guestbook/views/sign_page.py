b25195bb75f65b8715e905ade6bb198adf0c91a5
duylk <duylk@aoi-sys.vn>
2017-05-11 14:50:53 +0700

    check user before post guestbook

--- guestbook/views/sign_page.py
+++ guestbook/views/sign_page.py
@@ -45,9 +45,7 @@ class SignView(FormView):
 		def put_greeting():
 			greeting.put()
 
+		put_greeting()
 		if users.get_current_user():
-			put_greeting()
 			using_task_queue.add_task_queue(greeting.author, greeting.content)
-		else:
-			put_greeting()
 		return super(SignView, self).form_valid(form, **kwargs)
